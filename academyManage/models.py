b21837c0c622db6aad80022d45c260c049872c40
Pablo Yuri Scherer de Souza <pabloyuriss@hotmail.com>
2022-03-24 20:02:00 -0300

    Models needed for the first case

--- academyManage/models.py
+++ academyManage/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 # Create your models here.
 
+
 class User(models.Model):
     name = models.CharField(max_length=50)
     password = models.CharField(max_length=32)
     client_since = models.DateTimeField('Client Since')
+    userType = models.CharField(max_length=15) # Tipo do ator (Administrador,Cliente,Profissional)
 
-class Servico(models.Model):
-    name = models.CharField(max_length=50)
+class consultation(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
     teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField('Scheduled consultation')
+    results = models.CharField(max_length=200)
+
+
+
+
