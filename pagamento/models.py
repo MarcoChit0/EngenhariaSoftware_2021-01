a505c3a3aca5bbc2c3541d167e437dec2159743c
Pablo Yuri Scherer de Souza <pabloyuriss@hotmail.com>
2022-04-15 01:11:13 -0300

    Mudancas feitas para trabalhar fora de casa

--- pagamento/models.py
+++ pagamento/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from usuario.models import Cliente, Medico, Professor
+
+class CartaoDeCredito(models.Model):
+    numero = models.CharField(max_length=16)
+    saldo = models.IntegerField(default=0)
+    validade = models.DateTimeField()
+    CVV = models.CharField(max_length=3)
+
+    class Meta:
+        abstract = True
+
+class Pagamento(models.Model):
+    cartao = models.ForeignKey(CartaoDeCredito, on_delete=models.CASCADE, null=True, blank=True)
+    data_hora = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    valor = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
